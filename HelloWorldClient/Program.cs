7d973caa9d99a86372f456cdd2e726095b6b7d2e
=tapos007 <=tapos.aa@gmail.com>
2018-03-10 01:40:42 +0600

    Add project files.

---
+++ HelloWorldClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HelloWorldServiceClient();
+            Console.WriteLine(client.GetMessage("tapos ghosh"));
+            Console.ReadKey();
+        }
+    }
+}
